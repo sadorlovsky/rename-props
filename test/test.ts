527215b577242bb6b1261b963a735665bcae5c07
Zach Orlovsky <sadorlovsky@gmail.com>
2020-02-08 12:50:56 +0300

    add more test

--- test/test.ts
+++ test/test.ts
@@ -10,3 +10,13 @@ test('rename properly', t => {
   const renamed = renameProps({ x: 1, y: 2 }, { x: 'z', y: 'w' })
   t.deepEqual(renamed, { z: 1, w: 2 })
 })
+
+test('partially rename', t => {
+  const renamed = renameProps({ x: 1, y: 2 }, { x: 'z' })
+  t.deepEqual(renamed, { z: 1, y: 2 })
+})
+
+test('skip if there are no such keys', t => {
+  const renamed = renameProps({ x: 1 }, { y: 'z' })
+  t.deepEqual(renamed, { x: 1 })
+})
